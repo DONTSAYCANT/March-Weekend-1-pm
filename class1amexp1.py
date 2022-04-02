faf8f5dec46e20ff63b0bfb0af869020aa461ade
PARTHIPAN KANNAN <66922169+DONTSAYCANT@users.noreply.github.com>
2022-04-02 15:23:58 +0530

    Add files via upload

---
+++ class1amexp1.py
@@ -0,0 +1,12 @@
+def func1(var1, var2):
+    return var1 ** var2
+
+
+def func2(var1, var2, var3):
+    return var1 + var2 - var3
+
+
+def func3():
+    val = input("val1:")
+    val2 = input("val2:")
+    return val + val2
